4dab60b87a236bf15cad5c94f70737faf042b4ac
dker2412 <51265701+dker2412@users.noreply.github.com>
2022-10-02 00:03:51 +0530

    Add files via upload

---
+++ main/hello.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	handler := http.NewServeMux()
+	handler.HandleFunc("/api/hello", Hello)
+	http.ListenAndServe("0.0.0.0:8080", handler)
+	// Listen to all request to port 8080.
+}
+
+func Hello(w http.ResponseWriter, r *http.Request) {
+	var i int
+	fmt.Print("Type a number: ")
+	fmt.Scan(&i)
+	fmt.Println("Your number is:", i)
+	// fmt.Fprintf(w, "Your number is:")
+	fmt.Fprintf(w, `Hello world`)
+}
